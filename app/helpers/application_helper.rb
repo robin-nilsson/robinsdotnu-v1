e084ea7e19f4ae68c9b6711128ea592bb8018970
robin-nilsson <robin@robins.nu>
2016-03-24 17:50:37 +0100

    Better meta tags. Add titles for every page for SEO using a new helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,8 @@ module ApplicationHelper
   def active_class(link_path)
     current_page?(link_path) ? 'active' : ""
   end
+
+  def title(page_title)
+  content_for :title, page_title.to_s
+  end
 end
